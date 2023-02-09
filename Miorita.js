6fe217f616f61e6a49aeddf35396421d6788c45f
A Cornel <cornel@programminggurus.ro>
2023-02-09 16:04:10 +0000

    Teme

---
+++ Miorita.js
@@ -0,0 +1,21 @@
+/*
+    Replicați funcționalitatea Mioriței, folosind obiecte.
+    Miorița se află într-o stână infinită.
+    Implementează metoda move() pentru a o face pe Miorița să se deplaseze în orice direcție
+*/
+
+const miorita = {
+    x: 0,
+    y: 0,
+    move: function(moveX, moveY) {
+
+    }
+};
+
+miorita.move(1, 3);
+console.log(miorita.x, miorita.y);
+// 1 3
+
+miorita.move(1, -4);
+console.log(miorita.x, miorita.y);
+// 2 -1
