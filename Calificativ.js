c770a0a1e5f4e809ab2487b0bbf890edd4c886fa
mth97 <46069839+mth97@users.noreply.github.com>
2023-02-14 18:02:12 +0200

    Add files via upload

--- Calificativ.js
+++ Calificativ.js
@@ -14,4 +14,30 @@
      2: IS
      1: IS
 */
+function catalog(nota) {
+  if (nota > 0 && nota < 5) {
+    return "IS";
+  }
+  if (nota > 4 && nota < 7) {
+    return "S";
+  }
+  if (nota > 6 && nota < 9) {
+    return "B";
+  }
+  if (nota > 8 && nota < 11) {
+    return "FB";
+  } else return "Ai introdus o valoare gresita!";
+}
 
+console.log(catalog(78));
+console.log(catalog(1));
+console.log(catalog(2));
+console.log(catalog(3));
+console.log(catalog(4));
+console.log(catalog(5));
+console.log(catalog(6));
+console.log(catalog(7));
+console.log(catalog(8));
+console.log(catalog(9));
+console.log(catalog(10));
+console.log(catalog(-1));
