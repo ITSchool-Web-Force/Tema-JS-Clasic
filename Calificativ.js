d8cf8af9ee3442df475e63d5d58d4ab8f7adee3c
Alessia Rădos <rados.alessia.elena@gmail.com>
2023-02-16 17:48:14 +0200

    Tema calificativ

--- Calificativ.js
+++ Calificativ.js
@@ -15,3 +15,10 @@
      1: IS
 */
 
+function cal_from_nr(number)
+{
+    var cal = {10: 'FB', 9: 'FB', 8: 'B', 7: 'B', 6: 'S', 5: 'S', 4: 'IS', 3: 'IS', 2: 'IS', 1: 'IS'}
+    return cal[number]
+}
+
+console.log(cal_from_nr(5))
