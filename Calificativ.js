a91e6ff36d4e50e2e03c83921bfdef2e25875846
Cervencu George <120504020+GGC911@users.noreply.github.com>
2023-02-21 11:45:37 +0200

    calificativ done

--- Calificativ.js
+++ Calificativ.js
@@ -14,4 +14,11 @@
      2: IS
      1: IS
 */
-
+function gradeExchange(grade) {
+  if (grade === 10 || grade === 9) return "FB";
+  if (grade === 8 || grade === 7) return "B";
+  if (grade === 6 || grade === 5) return "S";
+  if (grade > 0 && grade <= 4) return "IS";
+  return "Invalid number, please try again!";
+}
+console.log(gradeExchange(9));
